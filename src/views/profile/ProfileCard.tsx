4712a518ecc906a74c48fa8eb58e7be818b18cce
piotr.westphal <piotr.westphal@gmail.com>
2024-01-15 18:45:09 +0100

    KA-35 Update image variants model

--- src/views/profile/ProfileCard.tsx
+++ src/views/profile/ProfileCard.tsx
@@ -25,7 +25,7 @@ export const ProfileCard = ({dto}: ProfileCardProps) => {
     return (
         <Card sx={{m: 1}}>
             <CardHeader sx={{pl: 2, pr: 2, pt: 1, pb: 0}}
-                        avatar={dto.profileImage.thumbKey ? <Avatar src={dto.profileImage.thumbKey}/>: 'N/A'}
+                        avatar={dto.profileImage?.thmb.key ? <Avatar src={dto.profileImage.thmb.key}/>: 'N/A'}
                         titleTypographyProps={{variant: 'h6', sx: {fontWeight: 600}}}
                         subheaderTypographyProps={{fontSize: 12, fontWeight: 200}}
                         title={displayName}
